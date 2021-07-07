c082788c26d3a3264a431fdc092b6af9906496a5
DeviousLab <deviouslab@gmail.com>
2021-07-07 20:19:33 +0400

    Fixed a few bugs/mistypes

--- tic-tac-toe/tttgame.py
+++ tic-tac-toe/tttgame.py
@@ -1,9 +1,10 @@
 import time
+import math
 from tttplayer import HumanPlayer, RandomCPUPlayer
 
 class TicTacToe:
     def __init__(self):
-        self.board = [' ' for _ in range(9)]
+        self.board = self.make_board()
         self.current_winner = None
 
     def print_board(self):
@@ -11,6 +12,9 @@ class TicTacToe:
             print('| ' + ' | '.join(row) + ' |')
 
     @staticmethod
+    def make_board():
+        return [' ' for _ in range(9)]
+
     def print_board_nums():
         number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
         for row in number_board:
@@ -27,9 +31,9 @@ class TicTacToe:
 
     def make_move(self, square, letter):
         if self.board[square] == ' ':
-            self.board[square] == letter
+            self.board[square] = letter
             if self.winner(square, letter):
-                self.current_winner = winner
+                self.current_winner = letter
             return True
         return False
 
@@ -75,7 +79,6 @@ def play(game, x_player, o_player, print_game=True):
             if print_game:
                 print(letter + ' wins!')
             return letter
-
         letter = 'O' if letter == 'X' else 'X'
 
     time.sleep(0.8)
