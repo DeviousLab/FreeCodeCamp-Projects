863c246ed9dfb12c6faeeea43cbaaa73909e7142
DeviousLab <deviouslab@gmail.com>
2021-07-07 20:28:18 +0400

    Minor formatting

--- tic-tac-toe/tttgame.py
+++ tic-tac-toe/tttgame.py
@@ -2,18 +2,18 @@ import time
 import math
 from tttplayer import HumanPlayer, RandomCPUPlayer
 
-class TicTacToe:
+class TicTacToe():
     def __init__(self):
         self.board = self.make_board()
         self.current_winner = None
 
-    def print_board(self):
-        for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
-            print('| ' + ' | '.join(row) + ' |')
-
     @staticmethod
     def make_board():
         return [' ' for _ in range(9)]
+
+    def print_board(self):
+        for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
+            print('| ' + ' | '.join(row) + ' |')
 
     def print_board_nums():
         number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
